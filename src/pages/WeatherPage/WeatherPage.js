08e9950629eeba8c6d2dd75052df59532a3f9f1d
Victor Escalona <victorescalonag@gmail.com>
2020-01-30 16:54:52 -0500

    Search bar working

--- src/pages/WeatherPage/WeatherPage.js
+++ src/pages/WeatherPage/WeatherPage.js
@@ -7,31 +7,28 @@ import * as moment from 'moment';
 
 const WeatherPage = () => {
     let now = moment().format('LL');
-    console.log(now);
     const [weather, setWeather] = useState({
         data: null,
         loading: true
     })
-    const place = "Bucaramanga";
-    const fetchData = async () => {
-
-        const query = `https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=4b4754a65698bacce3396cc517e79381`;
-        const req = await fetch(query);
-        const data = await req.json();
-        let newState = []
-        for (let i = 0; i < data.list.length; i++) {
-            if (i % 8 === 0) {
-                newState.push(data.list[i]);
-            }
-        }
-
-        setWeather({ data: newState, dataFull: data, loading: false })
-
-    }
+    const [city, setCity] = useState("madrid");
 
     useEffect(() => {
+        const fetchData = async () => {
+
+            const query = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=4b4754a65698bacce3396cc517e79381`;
+            const req = await fetch(query);
+            const data = await req.json();
+            let newState = []
+            for (let i = 0; i < data.list.length; i++) {
+                if (i % 8 === 0) {
+                    newState.push(data.list[i]);
+                }
+            }
+            setWeather({ data: newState, dataFull: data, loading: false })
+        }
         fetchData();
-    }, [])
+    }, [city])
 
     const getIcon = (dayWeather) => {
         if (dayWeather === "Clear") {
@@ -45,41 +42,53 @@ const WeatherPage = () => {
         }
     }
 
+    const handleSearch = e => {
+        if (e.key === "Enter") {
+            let value = e.target.value;
+            let valueLowerCase = value.toLowerCase();
+            setCity(valueLowerCase);
+            setWeather({ loading: true })
+        }
+    }
     if (weather.loading === true) {
         return "Loading...";
     }
 
-
-    console.log("page", weather.data[0].main)
-    const weatherData = weather.data;
-    return (<>
-        <div className="container_page">
-            <div className="weather_page">
-                <div className="weather_left">
-                    <div className="weather_location">
-                        <p>{weather.dataFull.city.name}, {weather.dataFull.city.country}</p>
-                    </div>
-                    <div className="weather_date">
-                        <p>{now}</p>
-                    </div>
-                    <div className="weather_weather">
-                        <i className={getIcon(weatherData[0].weather[0].main)}></i>
-                    </div>
+    if (weather.loading === false) {
+        console.log("page", weather.data[0].main)
+        const weatherData = weather.data;
+        return (<>
+            <div className="container_page">
+                <div className="weather_search">
+                    <input type="text" name="search-weather" id="search-weather" placeholder="Search City... Example: Madrid,Es " onKeyPress={e => handleSearch(e)} />
                 </div>
-                <div className="weather_right">
-                    <div className="weather_max_temp">
-                        <p>{parseInt(weatherData[0].main.temp - 273)}°</p>
+                <div className="weather_page">
+                    <div className="weather_left">
+                        <div className="weather_location">
+                            <p>{weather.dataFull.city.name}, {weather.dataFull.city.country}</p>
+                        </div>
+                        <div className="weather_date">
+                            <p>{now}</p>
+                        </div>
+                        <div className="weather_weather">
+                            <i className={getIcon(weatherData[0].weather[0].main)}></i>
+                        </div>
                     </div>
-                    <div className="weather_min_temp">
-                        <p>{parseInt(weatherData[0].main.temp_min - 273)}°</p>
+                    <div className="weather_right">
+                        <div className="weather_max_temp">
+                            <p>{parseInt(weatherData[0].main.temp - 273)}°</p>
+                        </div>
+                        <div className="weather_min_temp">
+                            <p>{parseInt(weatherData[0].main.temp_min - 273)}°</p>
+                        </div>
                     </div>
-                </div>
-                <div className="weather_bottom">
-                    <WeatherCard data={weatherData} fullData={weather.dataFull} state={weather} />
+                    <div className="weather_bottom">
+                        <WeatherCard data={weatherData} fullData={weather.dataFull} state={weather} />
+                    </div>
                 </div>
             </div>
-        </div>
-    </>)
+        </>)
+    }
 }
 
 export default WeatherPage;
